d105d3be186f709c2d6398b286a732b37dffa7bf
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-13 00:16:16 -0500

    Working zooming landscape background

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import FloorAnimation from '@wufe/floor-animation';
 function App() {
   return (
     <div className="App"> 
-          <FloorAnimation />
+          <FloorAnimation
+            color="#ffa600"
+            mode={0}
+          />
    
     </div> 
 
