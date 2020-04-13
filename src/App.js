9876bc412a92a169feb28dce7d91371081298ea5
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-13 00:03:51 -0500

    Working dark motion background animation unscaled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import FloorAnimation from '@wufe/floor-animation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+          <FloorAnimation />
+   
+    </div> 
+
   );
 }
 
