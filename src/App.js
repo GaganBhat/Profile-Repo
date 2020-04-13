7f13092ec65f9d8d8e1c89eba3e90d02d2dc8113
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-13 00:54:11 -0500

    Working particles and darkscape

--- src/App.js
+++ src/App.js
@@ -6,13 +6,39 @@ import FloorAnimation from '@wufe/floor-animation';
 
 function App() {
   return (
-    <div className="App"> 
-          <FloorAnimation
-            color="#ffa600"
-            mode={0}
+    <body style={{ backgroundColor: "#ffa600" }}>
+        <div className="App">
+          <Particles
+            params={{
+              particles: {
+                number: {
+                  value: 50
+                },
+                size: {
+                  value: 3
+                },
+                color: {
+                  value: "#000000"
+                }
+              },
+              background: {
+                color: {
+                  value: "#ffa600"
+                }
+              },
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "repulse"
+                  }
+                }
+              }
+            }}
           />
-   
-    </div> 
+          <FloorAnimation color="#ffa600" mode={0} />
+        </div>
+      </body>
 
   );
 }
