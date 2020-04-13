e68ed13d325f08a1551d4034226af0297eac860a
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-13 12:05:03 -0500

    Working text on particle effect

--- src/App.js
+++ src/App.js
@@ -6,39 +6,49 @@ import FloorAnimation from '@wufe/floor-animation';
 
 function App() {
   return (
-    <body style={{ backgroundColor: "#ffa600" }}>
-        <div className="App">
-          <Particles
-            params={{
-              particles: {
-                number: {
-                  value: 50
-                },
-                size: {
-                  value: 3
-                },
-                color: {
-                  value: "#000000"
-                }
-              },
-              background: {
-                color: {
-                  value: "#ffa600"
-                }
-              },
-              interactivity: {
-                events: {
-                  onhover: {
-                    enable: true,
-                    mode: "repulse"
+    
+    <html>
+      <head>
+        <title>Gagan Bhat</title>
+      </head>
+      <body style={{ backgroundColor: "#ffa600"}}>
+      <div style={{ backgroundColor: "#ffa600"}} id="container">
+            <div id="navi">
+              <Particles
+                params={{
+                  particles: {
+                    number: {
+                      value: 50
+                    },
+                    size: {
+                      value: 3
+                    },
+                    color: {
+                      value: "#000000"
+                    }
+                  },
+                  background: {
+                    color: {
+                      value: "#ffa600"
+                    }
+                  },
+                  interactivity: {
+                    events: {
+                      onhover: {
+                        enable: true,
+                        mode: "repulse"
+                      }
+                    }
                   }
-                }
-              }
-            }}
-          />
-          <FloorAnimation color="#ffa600" mode={0} />
-        </div>
-      </body>
+                }}
+              >
+              </Particles>
+              {/* <FloorAnimation color="#ffa600" mode={0} /> */}
+            </div>
+            <div className="centered">Bruh</div>
+          </div>
+        </body>
+      </html>
 
   );
 }
