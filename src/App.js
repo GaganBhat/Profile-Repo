711ebaf059b62804d2d25483048a1cb59f051b1c
Gagan Bhat <gaganbhat6@gmail.com>
2020-06-13 10:27:50 -0500

    Improved stylization, body, and color

--- src/App.js
+++ src/App.js
@@ -29,72 +29,55 @@ const TITLE = "Gagan Bhat"
 
 function App() {
   return (
-    
-    <html>
-      <head>
-        <Helmet>
-            <title>{ TITLE }</title>
-        </Helmet>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" 
-              rel="stylesheet" 
-              integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" 
-              crossorigin="anonymous">
-        </link>
-      </head>
-      <body>
-      <div id="container">
-            <div id="navi">
-              <Particles
-                params={{
-                  particles: {
-                    number: {
-                      value: 40
-                    },
-                    size: {
-                      value: 3
-                    },
-                  },
-                  background: {
-                    image: "linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)"
-                  },
-                  interactivity: {
-                    events: {
-                      onhover: {
-                        enable: true,
-                        mode: "repulse"
-                      }
-                    }
-                  },
-                  line_linked: {
-                    color: "#ffffff",
-                    opacity: 0.1
-                  }
-                }}
-              >
-              </Particles>
-              
-              <div className="mainflex">
-                <h1 className="centered">Gagan Bhat</h1>            
-                <ul id="nav-links">
-                  <li>
-                    <FontAwesomeIcon className="icon" icon={faLinkedin} color="#ffffff" size="2x" onClick={handleLinkedin}/>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon className="icon" icon={faGithub} color="#ffffff" size="2x" onClick={handleGithub}/>
-                  </li>
-                  <li>
-                    <FontAwesomeIcon className="icon" icon={faEnvelope} color="#ffffff" size="2x"onClick={handleMail}/>
-                  </li>
-                </ul>
-                
-              </div>
-            </div>
-          </div>
-        </body>
-      </html>
+    <div className="App">
+      <div id="particles">
+        <Particles
+          params={{
+            particles: {
+              opacity: 1,
+              color: "#ed7e48",
+              number: {
+                value: 100
+              },
+              size: {
+                value: 5
+              },
+              line_linked: {
+                distance: 200,
+              }
+            },
+            background: {
+              image: "linear-gradient(160deg, #d53369, #cbad6d)"
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                }
+              }
+            }
+          }}
+        >
+        </Particles>
+      </div>
+      <div className="mainflex">
+        <h1 className="centered" style={{ top: '-55px', color: '#ffffff' }}>Gagan Bhat</h1>
+        <ul id="nav-links">
+          <li>
+            <FontAwesomeIcon className="icon" cursor="grab" icon={faLinkedin} color="#ffffff" size="2x" onClick={handleLinkedin} />
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" cursor="grab" icon={faGithub} color="#ffffff" size="2x" onClick={handleGithub} />
+          </li>
+          <li>
+            <FontAwesomeIcon className="icon" cursor="grab" icon={faEnvelope} color="#ffffff" size="2x" onClick={handleMail} />
+          </li>
+        </ul>
+      </div>
 
+    </div>
   );
-  
 }
 
 
